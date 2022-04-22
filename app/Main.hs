4ad3ee3b6f228255196c584d9a322082ca5214fb
Kowsar Atazadeh <kowsar.atazadeh@gmail.com>
2022-04-22 20:19:57 +0430

    Add ecrecover function

--- app/Main.hs
+++ app/Main.hs
@@ -1,40 +1,20 @@
 module Main where
 
-import           SchnorrSecp256K1          (hexToBytes, messageHash,
-                                            serializedSignature,
-                                            uncompressedPublicKey,
-                                            verifySignature, publicKeyToEthAddress)
+import           SchnorrSecp256K1          (hexToBytes, ecrecover, getEthAddress)
 import           SchnorrSecp256K1.Internal (contextCreate, verifyContext)
 
-testVerify :: IO ()
-testVerify = do
+testEcrecover :: IO ()
+testEcrecover = do
   ctx <- contextCreate verifyContext
-  case serializedSignature serSigBS of
-    Nothing -> error "An error occurred while reading signature"
-    Just serSig -> case messageHash msgHashBS of
-      Nothing -> error "An error occurred while reading message hash"
-      Just msgHash -> case uncompressedPublicKey pubKeyBS of
-        Nothing -> error "An error occurred while reading public key"
-        Just pubKey -> case verifySignature ctx serSig msgHash pubKey of
-          Nothing -> error "An error occurred while verifying signature"
-          Just verified ->
-            if verified
-              then print "Signature is valid"
-            else
-              print "Signature not valid"
+  case ecrecover ctx (fromIntegral rawHash) (fromIntegral 27) (fromIntegral r) (fromIntegral s) of
+    Nothing -> error "An error occurred while recovering eth address"
+    Just ethAddr -> if (getEthAddress ethAddr) == ethAddrBS then print "ETH ADDRESSES MATCH!" else 
+        error "An error occurred while recovering eth address"
   where
-    serSigBS = hexToBytes "ee241b0e406661d43c1729a70ad0d7ef90cd13ad60c1065be0823196e23980270d0213d2af3f92478026381c567a2532b08ad06c0b349c7e9318ade44a9c2d0a00"
-    msgHashBS = hexToBytes "9902e8fe012a92c68276dfb7584a339587f8ffa859357e5a26a96af33ee25346"
-    pubKeyBS = hexToBytes "043d5c2875c9bd116875a71a5db64cffcb13396b163d039b1d932782489180433476a4352a2add00ebb0d5c94c515b72eb10f1fd8f3f03b42f4a2b255bfc9aa9e3"
-
-testPubKeyToEthAddress :: IO ()
-testPubKeyToEthAddress = case uncompressedPublicKey pubKeyBS of
-  Nothing -> error "An error occurred while parsing public key"
-  Just upk -> case publicKeyToEthAddress upk of
-    Nothing -> error "An error occurred while getting eth address from public key"
-    Just ea -> print ea
-  where
-    pubKeyBS = hexToBytes "043d5c2875c9bd116875a71a5db64cffcb13396b163d039b1d932782489180433476a4352a2add00ebb0d5c94c515b72eb10f1fd8f3f03b42f4a2b255bfc9aa9e3"
+    r = 0xee241b0e406661d43c1729a70ad0d7ef90cd13ad60c1065be0823196e2398027
+    s = 0x0d0213d2af3f92478026381c567a2532b08ad06c0b349c7e9318ade44a9c2d0a
+    rawHash = 0x9902e8fe012a92c68276dfb7584a339587f8ffa859357e5a26a96af33ee25346
+    ethAddrBS = hexToBytes "b9beb1d72e322de4b1f5f14abdbdaab50e310924"
 
 main :: IO ()
-main = testPubKeyToEthAddress
+main = testEcrecover
--- src/SchnorrSecp256K1.hs
+++ src/SchnorrSecp256K1.hs
@@ -1,13 +1,18 @@
-module SchnorrSecp256K1 (hexToBytes, verifySignature, serializedSignature, messageHash, uncompressedPublicKey, publicKeyToEthAddress) where
+module SchnorrSecp256K1 (hexToBytes, ecrecover, getEthAddress) where
 
 import           Crypto.Hash.Keccak        (keccak256)
+import           Data.Binary               (Word16, Word32, Word8, encode)
+import qualified Data.Binary.Get           as B
 import           Data.ByteString           (ByteString)
 import qualified Data.ByteString           as BS
 import qualified Data.ByteString.Base16    as B16
 import qualified Data.ByteString.Char8     as B8
+import           Data.ByteString.Lazy      (toStrict)
 import qualified Data.ByteString.Unsafe    as BU
+import           Data.DoubleWord           (Word128, Word256, hiWord, loWord)
 import           Data.Either               (fromRight)
-import           Foreign                   (Ptr, Storable (peek, poke), alloca,
+import           Foreign                   (Bits (shiftR), Ptr,
+                                            Storable (peek, poke), alloca,
                                             allocaBytes, castPtr)
 import           Foreign.C                 (CSize (..))
 import           SchnorrSecp256K1.Internal (Context, ecPubkeySerialize,
@@ -34,7 +39,22 @@ newtype SerializedSignature = SerializedSignature {getSerializedSignature :: Byt
 newtype PublicKey = PublicKey {getPublicKey :: ByteString}
 newtype UncompressedPublicKey = UncompressedPublicKey {getUncompressedPublicKey :: ByteString} deriving Eq
 newtype MessageHash = MessageHash {getMessageHash :: ByteString}
-newtype EthAddress = EthAddress {getEthAddress :: ByteString}
+newtype EthAddress = EthAddress {getEthAddress :: ByteString} deriving Eq
+
+instance Show CompactSignature where
+    showsPrec _ = shows . B16.encodeBase16 . getCompactSignature
+instance Show RecoverableSignature where
+    showsPrec _ = shows . B16.encodeBase16 . getRecoverableSignature
+instance Show SerializedSignature where
+    showsPrec _ = shows . B16.encodeBase16 . getSerializedSignature
+instance Show PublicKey where
+    showsPrec _ = shows . B16.encodeBase16 . getPublicKey
+instance Show UncompressedPublicKey where
+    showsPrec _ = shows . B16.encodeBase16 . getUncompressedPublicKey
+instance Show MessageHash where
+    showsPrec _ = shows . B16.encodeBase16 . getMessageHash
+instance Show EthAddress where
+    showsPrec _ = shows . B16.encodeBase16 . getEthAddress
 
 compactSignature :: ByteString -> Maybe CompactSignature
 compactSignature bs
@@ -61,15 +81,11 @@ ethAddress bs
     | BS.length bs == 20 = Just (EthAddress bs)
     | otherwise = Nothing
 
--- TODO: remove?
-instance Show EthAddress where
-    showsPrec _ = shows . B16.encodeBase16 . getEthAddress
-
 parseSerializedSignature :: SerializedSignature -> Maybe (CompactSignature, Int)
 parseSerializedSignature serSig = do
         case compactSignature serSig64BS of
-          Nothing -> Nothing
-          Just cs -> Just (cs, fromIntegral recoverIdBS)
+            Nothing -> Nothing
+            Just cs -> Just (cs, fromIntegral recoverIdBS)
     where
         serSigBS = getSerializedSignature serSig
         recoverIdBS = BS.last serSigBS
@@ -77,21 +93,16 @@ parseSerializedSignature serSig = do
 
 deserializeRecoverable :: Context -> SerializedSignature -> Maybe RecoverableSignature
 deserializeRecoverable context serializedSig = do
-    case parseSerializedSignature serializedSig of
-        Nothing -> Nothing
-        Just parsedSerializedSig -> do
-            unsafePerformIO $ unsafeUseByteString (getCompactSignature compactedSig) $ \(compactedSigIn, _) -> do
-                allocaBytes 65 $ \recoverableSigOut -> do
-                    result <- parseCompactRecoverableSignature context recoverableSigOut compactedSigIn (fromIntegral recoveryId)
-                    if isSuccess result
-                        then do
-                            bs <- packByteString (recoverableSigOut, 65)
-                            return (Just (RecoverableSignature bs))
-                        else do
-                            return Nothing
-            where
-                compactedSig = fst parsedSerializedSig
-                recoveryId = snd parsedSerializedSig
+    parsedSerializedSig <- parseSerializedSignature serializedSig
+    unsafePerformIO $ unsafeUseByteString (getCompactSignature (fst parsedSerializedSig)) $ \(compactedSigIn, _) -> do
+        allocaBytes 65 $ \recoverableSigOut -> do
+            result <- parseCompactRecoverableSignature context recoverableSigOut compactedSigIn (fromIntegral (snd parsedSerializedSig))
+            if isSuccess result
+                then do
+                    bs <- packByteString (recoverableSigOut, 65)
+                    return (Just (RecoverableSignature bs))
+                else do
+                    return Nothing
 
 serializePublicKey :: Context -> PublicKey -> Maybe UncompressedPublicKey
 serializePublicKey context pubKey = unsafePerformIO $
@@ -125,25 +136,36 @@ recover context recoverableSig msgHash = unsafePerformIO $
 
 verifySignature :: Context -> SerializedSignature -> MessageHash -> UncompressedPublicKey -> Maybe Bool
 verifySignature context serializedSig msgHash referencePubKey = do
-    case deserializeRecoverable context serializedSig of
-        Nothing -> Nothing
-        Just recoverableSig -> do
-            publicKey <- recover context recoverableSig msgHash
-            case serializePublicKey context publicKey of
-                Nothing -> Nothing
-                Just uncompressedPubKey -> Just (referencePubKey == uncompressedPubKey)
-
+    recoverableSig <- deserializeRecoverable context serializedSig
+    publicKey <- recover context recoverableSig msgHash
+    uncompressedPubKey <- serializePublicKey context publicKey
+    Just (referencePubKey == uncompressedPubKey)
 
 getTailOfLength :: ByteString -> Int -> Maybe ByteString
 getTailOfLength bs length
-  | length < 0 = Nothing
-  | length > bsLength = Nothing
-  | length == bsLength = Just bs
-  | otherwise = getTailOfLength (BS.tail bs) length
+    | length < 0 = Nothing
+    | length > bsLength = Nothing
+    | length == bsLength = Just bs
+    | otherwise = getTailOfLength (BS.tail bs) length
   where
-      bsLength = BS.length bs
+        bsLength = BS.length bs
 
 publicKeyToEthAddress :: UncompressedPublicKey -> Maybe EthAddress
 publicKeyToEthAddress pubKey = ethAddress =<< getTailOfLength pubKeyHash 20
     where
         pubKeyHash = keccak256 $ BS.tail $ getUncompressedPublicKey pubKey
+
+encode128 :: Word128 -> ByteString
+encode128 word128 = BS.append (toStrict $ encode $ hiWord word128) (toStrict $ encode $ loWord word128)
+
+encode256 :: Word256 -> ByteString
+encode256 word256 = BS.append (encode128 $ hiWord word256) (encode128 $ loWord word256)
+
+ecrecover :: Context -> Word256 -> Word8 -> Word256 -> Word256 -> Maybe EthAddress
+ecrecover context rawHash v r s = do
+    recoverableSig <- deserializeRecoverable context sig
+    publicKey <- recover context recoverableSig msgHash
+    publicKeyToEthAddress =<< serializePublicKey context publicKey
+    where
+        sig = SerializedSignature $ BS.append (BS.append (encode256 r) (encode256 s)) (toStrict $ encode $ v - 27)
+        msgHash = MessageHash $ encode256 rawHash
